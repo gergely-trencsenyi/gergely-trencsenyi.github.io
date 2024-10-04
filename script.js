0a1f93f99e2fd86f0f6da35926ecae438457a964
gergely-trencsenyi <gergely.trencsenyi@edu.bme.hu>
2024-10-04 12:43:30 +0200

    mobile first

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 function ChangePage() { const divs = document.querySelectorAll("div"); //Minden eltüntetése
     divs.forEach(div => {
-        div.style.display = "none"})
+        div.style.display = "none";
+        document.getElementById("bottom").style.display = "none";})
     }
 
 function Home() { document.getElementById("home").style.display = "block"; }; //Kiválasztott div megjelenítése
 function Portfolio() { document.getElementById("pf").style.display = "block"; };
 function Galeria() { document.getElementById("gal").style.display = "block"; };
 function About() { document.getElementById("abt").style.display = "block"; };
+function Bottom () { document.getElementById("bottom").style.display = "block"; };
 function proba() {alert('PROBA');};
 
 const pf_btn = document.getElementById("pf_btn");
 pf_btn.addEventListener('click', () => {
     ChangePage();
     Portfolio();
+    Bottom();
   });
 
 const gal_btn = document.getElementById("gal_btn");
 gal_btn.addEventListener('click', () => {
     ChangePage();
     Galeria();
+    Bottom();
   });
 
 const abt_btn = document.getElementById("abt_btn");
 abt_btn.addEventListener('click', () => {
     ChangePage();
     About();
+    Bottom();
   });
 
 const hom_btn = document.querySelectorAll('.hom_btn');
